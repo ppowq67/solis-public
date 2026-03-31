96bea1c82f500930c74f478b12a404c24c888999
ppowq67 <ppowq23@gmail.com>
2026-04-01 00:17:39 +0400

    wqer

--- js/s_new/procc.js
+++
@@ -1,4 +0,0 @@
-function _0x1b473e() {
-    const _0x46d929 = document['getElementById']('youtubeUrlInput');
-    _0x46d929 && window['clipStudio'] && typeof window['clipStudio']['processYouTubeUrl'] === 'function' && window['clipStudio']['processYouTubeUrl']();
-}
